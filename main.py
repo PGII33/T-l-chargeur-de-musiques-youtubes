c31441758a78ce819cb5e8006cd14afdf4271b5a
PGII33 <mickycomptepro@gmail.com>
2023-11-19 10:14:46 +0100

    Fixed errors

--- main.py
+++ main.py
@@ -2,12 +2,24 @@
 import os
 from pytube import YouTube
 from pytube import Playlist
+from tkinter import *
 
 RUNNING = False
 
 BG_ENTRY_COULEUR = '#000000'
 BG_COULEUR = '#8F8F8F'
 FG_COULEUR = '#FFFFFF'
+
+label_text = 'Welcome to the youtube downloader'
+label_download = None
+
+window = Tk()
+window.title("Youtube downloader")
+window.geometry("720x480")
+window.minsize(480, 360)
+
+v_choose = IntVar()
+entry_var = StringVar()
 
 def verify_link(link:str):
     ''' Verify if the link is a string \n
@@ -31,11 +43,18 @@ def get_music(link):
         print('link must be a youtube link')
     yt = YouTube(link)
     print(yt.title + ' is downloading...')
+    # Actualisation of the label
+    if label_download is not None:
+        label_text = yt.title + ' is downloading...'
+        label_download.config(text=label_text)
     audio_file = yt.streams.filter(only_audio=True).first().download('Musics_Downloads')
     base = os.path.splitext(audio_file)[0]
     new_file = base + '.mp3'
     os.rename(audio_file, new_file)
     print(yt.title + ' is downloaded')
+    if label_download != None:
+        label_text = yt.title + ' is downloaded'
+        label_download.config(text=label_text)
     return new_file
 
 # Get the vido of the link
@@ -44,8 +63,15 @@ def get_video(link) -> None:
     assert isinstance(link, str), 'link must be a string'
     yt = YouTube(link)
     print(yt.title + ' is downloading...')
+    if label_download != None:
+        
+        label_text = yt.title + ' is downloading...'
+        label_download.config(text=label_text)
     video_file = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download('Musics_Downloads') # pylint: disable=line-too-long
     print(yt.title + ' is downloaded')
+    if label_download != None:
+        label_text = yt.title + ' is downloading...'
+        label_download.config(text=label_text)
     return video_file
 
 # Get the music playlist of the link
@@ -83,8 +109,43 @@ def button_action()-> None:
         get_music_from_playlist(link)
     elif v_choose.get() == 3:
         get_video_from_playlist(link)
+    link_entry.delete(0, END)
+
 
 print("Bienvenue dans le programme de téléchargement de musique et de vidéo \n")
+
+# Create the main frame
+main_frame = Frame(window, bg=BG_COULEUR)
+main_frame.pack(expand=YES, fill=BOTH)
+
+# Create the title label
+label_title = Label(main_frame, text="Youtube downloader", font=("Arial", 40), bg=BG_COULEUR, fg=FG_COULEUR)
+label_title.pack()
+
+# Create the link entry
+link_entry = Entry(main_frame, font=("Arial", 20), bg=BG_COULEUR, fg=FG_COULEUR, textvariable= entry_var)
+link_entry.pack()
+
+# Create the choice button
+choice_music = Radiobutton(main_frame, text="Music", font=("Arial", 20), bg=BG_COULEUR, variable=v_choose, value=0)
+choice_video = Radiobutton(main_frame, text="Video", font=("Arial", 20), bg=BG_COULEUR, variable=v_choose, value=1)
+choice_music_playlist = Radiobutton(main_frame, text="Music playlist", font=("Arial", 20), bg=BG_COULEUR, variable=v_choose, value=2)
+choice_video_playlist = Radiobutton(main_frame, text="Video playlist", font=("Arial", 20), bg=BG_COULEUR, variable=v_choose, value=3)
+choice_music.pack()
+choice_video.pack()
+choice_music_playlist.pack()
+choice_video_playlist.pack()
+
+# Create the button
+button = Button(main_frame, text="Download", font=("Arial", 20), bg=BG_COULEUR, fg=FG_COULEUR, command=button_action)
+button.pack()
+
+# Create label for the download
+label_download = Label(main_frame, text=label_text, font=("Arial", 20), bg=BG_COULEUR, fg=FG_COULEUR)
+label_download.pack()
+
+window.mainloop()
+
 # Here is for cmd
 while RUNNING :
     choose = int(input("Choisissez l'option \n"+
@@ -113,42 +174,3 @@ while RUNNING :
             get_music(a)
         except :
             print("Erreur")
-
-# Here is for the GUI
-from tkinter import *
-
-window = Tk()
-window.title("Youtube downloader")
-window.geometry("720x480")
-window.minsize(480, 360)
-
-v_choose = IntVar()
-entry_var = StringVar()
-
-# Create the main frame
-main_frame = Frame(window, bg=BG_COULEUR)
-main_frame.pack(expand=YES, fill=BOTH)
-
-# Create the title label
-label_title = Label(main_frame, text="Youtube downloader", font=("Arial", 40), bg=BG_COULEUR, fg=FG_COULEUR)
-label_title.pack()
-
-# Create the link entry
-link_entry = Entry(main_frame, font=("Arial", 20), bg=BG_COULEUR, fg=FG_COULEUR, textvariable= entry_var)
-link_entry.pack()
-
-# Create the choice button
-choice_music = Radiobutton(main_frame, text="Music", font=("Arial", 20), bg=BG_COULEUR, variable=v_choose, value=0)
-choice_video = Radiobutton(main_frame, text="Video", font=("Arial", 20), bg=BG_COULEUR, variable=v_choose, value=1)
-choice_music_playlist = Radiobutton(main_frame, text="Music playlist", font=("Arial", 20), bg=BG_COULEUR, variable=v_choose, value=2)
-choice_video_playlist = Radiobutton(main_frame, text="Video playlist", font=("Arial", 20), bg=BG_COULEUR, variable=v_choose, value=3)
-choice_music.pack()
-choice_video.pack()
-choice_music_playlist.pack()
-choice_video_playlist.pack()
-
-# Create the button
-button = Button(main_frame, text="Download", font=("Arial", 20), bg=BG_COULEUR, fg=FG_COULEUR, command=button_action)
-button.pack()
-
-window.mainloop()

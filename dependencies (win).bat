12618ce0fdc136470511512c9b1c63e474dba2b7
Micky <micky.mari@etu.u-bordeaux.fr>
2024-04-27 18:50:14 +0200

    fix dependencies (win).bat :  - now successuly install the requirments

--- dependencies (win).bat
+++ dependencies (win).bat
@@ -2,4 +2,4 @@ winget install -e --id Python.Python.3.11
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 C:\> python3.11 get-pip.py
 rm get-pip.py
-C:\> pip install -r requirements.txt
+pip install -r requirements.txt

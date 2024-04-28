6a7daf352979c43efbceab57da2a0999e8e683a4
Micky <micky.mari@etu.u-bordeaux.fr>
2024-04-28 20:19:06 +0200

    updt yt_dl.py :  - Correction convention

--- YT/yt_dl.py
+++ YT/yt_dl.py
@@ -3,11 +3,12 @@ import os
 from pytube import YouTube
 from pytube import Playlist
 
+
 # Verify if the link is a youtube link
-def verify_link(link:str) -> str | bool:
-    ''' Verify if the link is a string \n
-    Return the link if it's a youtube or a youtube music link  \n
-    Return False if it's not a youtube or a youtube music link'''
+def verify_link(link: str) -> str | bool:
+    """ Verify if the link is a string \n
+    Return the link if it's a YouTube or a YouTube music link  \n
+    Return False if it's not a YouTube or a YouTube music link"""
 
     # Vérification du type de la variable link
     try:
@@ -36,11 +37,12 @@ def verify_link(link:str) -> str | bool:
         print('Le lien doit être un lien youtube ou youtube music')
         return False
 
+
 # Download the music from the link
 def get_music(link) -> bool:
-    ''' Download the music from a link \n
+    """ Download the music from a link \n
     Return True if it's downloaded \n
-    Return False if it's not'''
+    Return False if it's not"""
 
     # Vérification du type de la variable link
     try:
@@ -80,9 +82,10 @@ def get_music(link) -> bool:
     print(yt.title + ' is downloaded')
     return True
 
+
 # Get the music playlist of the link
 def get_music_from_playlist(link):
-    ''' Download the musics from a playlist '''
+    """ Download the musics from a playlist """
     assert isinstance(link, str), 'link must be a string'
     playlist = Playlist(link)
     counter = 1
@@ -92,20 +95,23 @@ def get_music_from_playlist(link):
         print(str(counter) + ' ' + urls + ' is downloaded')
         counter += 1
 
+
 # Get the vido of the link
 def get_video(link) -> None:
-    ''' Download the video from a link '''
+    """ Download the video from a link """
     assert isinstance(link, str), 'link must be a string'
     yt = YouTube(link)
     print(yt.title + ' is downloading...')
     target_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Musics_Download'))
-    video_file = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(target_directory) # pylint: disable=line-too-long
+    video_file = yt.streams.filter(progressive=True, file_extension='mp4').order_by(
+        'resolution').desc().first().download(target_directory)  # pylint: disable=line-too-long
     print(yt.title + ' is downloaded')
     return video_file
 
+
 # Get the video playlist of the link
-def get_video_from_playlist(link:str) -> None:
-    ''' Download the video from a playlist '''
+def get_video_from_playlist(link: str) -> None:
+    """ Download the video from a playlist """
     assert isinstance(link, str), 'link must be a string'
     playlist = Playlist(link)
     counter = 0

ab97cbb642b75a907f8ae2cfabf5263febc05c32
Micky <129859627+PGII33@users.noreply.github.com>
2024-04-29 10:50:30 +0200

    Update dependencies (win).bat
    - supp some lines and do some modifs

--- dependencies (win).bat
+++ dependencies (win).bat
@@ -1,5 +1,4 @@
 winget install -e --id Python.Python.3.11
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-C:\> python3.11 get-pip.py
-rm get-pip.py
+python3.11 get-pip.py
 pip install -r requirements.txt
